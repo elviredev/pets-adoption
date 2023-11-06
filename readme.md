a289633febbb8f0195ca03a7a003637001ecaaf9
elviredev <sandrine.rodriguez@hotmail.com>
2023-11-06 15:39:16 +0100

    add filter and media queries

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Pets Adoption
+- JavaScript Projet
+- Front et Back
+
+- Deployer sur GitHub
+- Utiliser Edge ou Firefox
+
+## FRONT
+- HTML - CSS - JS
+> GitHub : https://github.com/elviredev/pets-adoption
+
+## BACK
+- Pages détails d'un animal
+- Photos
+- pets.json
+> GitHub : https://github.com/elviredev/pets-adoption-data
+
+<img src="screenshot-page.png" width="951" style="display: block;margin: 40px auto"/>
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /**
  * Mon API est créée sur GitHub Pages pour récupérer les datas
+ * https://github.com/elviredev/pets-adoption-data
  * API d'origine : https://learnwebcode.github.io/pet-adoption-data/pets.json
  * @type {Response}
  */
@@ -19,6 +20,9 @@ function decideAgeText(age) {
     return age > 1 ? `${age} ans` : "1 an"
 }
 
+/*
+    Peupler index.html avec les datas
+*/
 pets.forEach(pet => {
     // Ajouter un clone du template
     const clone = template.content.cloneNode(true)
@@ -47,8 +51,75 @@ pets.forEach(pet => {
     // lien vers page détail d'un animal
     clone.querySelector('.btn-primary').href = `https://elviredev.github.io/pets-adoption-data/pets/${pet.id}/`
 
-
     wrapper.appendChild(clone)
 })
 
 document.querySelector('.animals').appendChild(wrapper)
+
+/*
+    Filtres
+*/
+
+const filterButtons = document.querySelectorAll('.filter-nav a')
+filterButtons.forEach(el => {
+    el.addEventListener("click", e => handleFilterClick(e))
+})
+
+function handleFilterClick(e) {
+    let target = e.target
+
+    if (e.target.classList.contains("only-large-screen")) {
+        target = e.target.closest("a")
+    }
+
+    e.preventDefault()
+    // Supprimer active class sur tous les liens
+    filterButtons.forEach(el => {
+        el.classList.remove("active")
+    })
+    // Ajouter active class sur le lien cliqué
+    target.classList.add("active")
+
+    // Fonction de filtrage selon l'espèce
+    //En paramètre target.dataset.filter = all, dog, cat, rabbit
+    filterPets(target.dataset.filter)
+}
+
+function filterPets(species) {
+    const allPets = document.querySelectorAll('.animal-card')
+    if (species == "all") {
+        allPets.forEach(el => {
+            el.style.display = ""
+        })
+    } else {
+        allPets.forEach(el => {
+            if (el.querySelector(".species").textContent == species) {
+                el.style.display = ""
+            } else {
+                el.style.display = "none"
+            }
+        })
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
